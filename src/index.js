6763dfbcd709b7d1c448c6e15a3a19b33582d818
helena-yuzhnaya <40338865+helena-yuzhnaya@users.noreply.github.com>
2020-10-20 01:44:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+     if(n < 0 ){ 
+         n = n.toString().substr(1,n.length);
+     }
+    let solution = n.toString().split('').reverse().join('');
+    return Number(solution);
 }
